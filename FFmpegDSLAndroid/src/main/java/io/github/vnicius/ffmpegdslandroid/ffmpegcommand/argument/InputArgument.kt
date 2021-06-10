7c435d5e22f3f580568842bfa9d88474e2a43fa3
Vinícius <vinicius.matheus252@gmail.com>
2021-06-10 18:02:33 -0300

    Fix InputArgument quotes on path

--- FFmpegDSLAndroid/src/main/java/io/github/vnicius/ffmpegdslandroid/ffmpegcommand/argument/InputArgument.kt
+++ FFmpegDSLAndroid/src/main/java/io/github/vnicius/ffmpegdslandroid/ffmpegcommand/argument/InputArgument.kt
@@ -4,5 +4,5 @@ import io.github.vnicius.ffmpegdslandroid.ffmpegcommand.argument.Argument
 
 class InputArgument(path: String): Argument() {
     override val key: String = "-i"
-    override val value: String = path
+    override val value: String = "\"$path\""
 }
