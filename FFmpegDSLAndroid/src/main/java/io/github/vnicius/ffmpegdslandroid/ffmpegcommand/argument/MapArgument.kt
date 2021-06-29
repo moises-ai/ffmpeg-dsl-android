bf3c4057a73bd74751927b956a18e354942d783d
Vinícius <vinicius.matheus252@gmail.com>
2021-06-29 20:35:06 -0300

    Add Map Argument test

--- FFmpegDSLAndroid/src/main/java/io/github/vnicius/ffmpegdslandroid/ffmpegcommand/argument/MapArgument.kt
+++ FFmpegDSLAndroid/src/main/java/io/github/vnicius/ffmpegdslandroid/ffmpegcommand/argument/MapArgument.kt
@@ -1,12 +1,18 @@
 package io.github.vnicius.ffmpegdslandroid.ffmpegcommand.argument
 
+import io.github.vnicius.ffmpegdslandroid.ffmpegcommand.assertion.StreamSpecifierAssertion
+
 
 /**
  * Created by Vinícius Veríssimo on 25/06/21.
  * github: @vnicius
  * vinicius.matheus252@gmail.com
  */
-class MapArgument(private val streamSpecifier: String): Argument() {
+class MapArgument(streamSpecifier: String): Argument() {
     override val key: String = "-map"
     override val value: String = streamSpecifier
+
+    init {
+        StreamSpecifierAssertion.assert(streamSpecifier)
+    }
 }
