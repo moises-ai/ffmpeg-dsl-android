efb47b69dd034f880a932bf1182e251df38cecb2
Vinícius <vinicius.matheus252@gmail.com>
2021-07-01 09:59:56 -0300

    Add OutputPathValue unit test

--- FFmpegDSLAndroid/src/main/java/io/github/vnicius/ffmpegdslandroid/ffmpegcommand/value/Value.kt
+++ FFmpegDSLAndroid/src/main/java/io/github/vnicius/ffmpegdslandroid/ffmpegcommand/value/Value.kt
@@ -1,11 +1,17 @@
 package io.github.vnicius.ffmpegdslandroid.ffmpegcommand.value
 
+import io.github.vnicius.ffmpegdslandroid.ffmpegcommand.assertion.ValueAssertion
 import io.github.vnicius.ffmpegdslandroid.ffmpegcommand.command.CommandArgumentParser
 import io.github.vnicius.ffmpegdslandroid.ffmpegcommand.command.ValueCommandArgument
 
 abstract class Value: ValueCommandArgument, CommandArgumentParser {
-    override fun parseToString(): String =
-        "\"${value.trim()}\""
+    override fun parseToString(): String {
+        val trimmedValue = value.trim()
+
+        ValueAssertion.assert(trimmedValue)
+
+        return "\"${trimmedValue}\""
+    }
 
     override fun toString(): String =
         parseToString()
